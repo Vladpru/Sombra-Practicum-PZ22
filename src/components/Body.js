d276ab4103438f9a90d18fd17de71826383cda7e
Vladpru <vladllen96681@gmail.com>
2025-12-10 11:17:13 +0200

    feat

--- src/components/Body.js
+++ src/components/Body.js
@@ -23,7 +23,7 @@ const Body = () => {
       </div>
       <div style={styles.card}>
         <img
-          src={"/DenysS.jpg"}
+          src={ "/DenysS.jpg" }
           alt="Денис Сич"
           style={styles.photo}
         />
