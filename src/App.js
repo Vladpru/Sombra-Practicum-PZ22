193daf64f76bb0978113a81499406a6688f8e114
Vladpru <vladllen96681@gmail.com>
2025-12-12 00:21:40 +0200

    add error for demostration

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 
 function App() {
+  ERROR FOR DEMOSTRATION
   return (
     <div className="App">
       <Header />
